09588ec09da8b2031e00901390543ee1af78270f
Ezy Wallyker <nothinghere2408@gmail.com>
2024-10-01 15:14:42 -0300

    Create Q02.java

---
+++ Q02.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Q02 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um número:");
+        int numero = sc.nextInt();
+        if (numero % 2 == 0) {
+            System.out.println("O número digitado é par.");
+        } else {
+            System.out.println("O número digitado é ímpar.");
+        }
+        if (numero > 0) {
+            System.out.println("O número é positivo.");
+        } else if (numero < 0) {
+            System.out.println("O número é negativo.");
+    }
+}
