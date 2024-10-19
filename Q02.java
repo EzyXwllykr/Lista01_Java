32b79bda83ba85fd618a6311d2688dea2f1df3cc
Ezy Wallyker <nothinghere2408@gmail.com>
2024-10-19 15:19:53 -0300

    Update Q02.java

--- Q02.java
+++ Q02.java
@@ -7,10 +7,19 @@ TURMA: TCTG241CNTDEV
 import java.util.Scanner;
 
 public class Q02 {
+    public static boolean isPrimo(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+        
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite um número:");
-        int numero = sc.nextInt();
+        int numero = scanner.nextInt();
+        
         if (numero % 2 == 0) {
             System.out.println("O número digitado é par.");
         } else {
@@ -20,5 +29,14 @@ public class Q02 {
             System.out.println("O número é positivo.");
         } else if (numero < 0) {
             System.out.println("O número é negativo.");
+        if (isPrimo(numero)) {
+            System.out.println("O número é primo.");
+        } else {
+            System.out.println("O número não é primo.");
+        }
+        System.out.println("A raiz quadrada do número é: " + Math.sqrt(numero));
+        System.out.println("O número elevado a 3 é igual a: " + Math.pow(numero, 3));
+
+        scanner.close();
     }
 }
